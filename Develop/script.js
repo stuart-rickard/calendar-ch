fe40f09a8f9b96c1be214e4d6611655db8e51b3e
stuart-rickard <stuart@bau-dev.com>
2021-12-05 08:44:42 -0800

    ready for final testing

--- Develop/script.js
+++ Develop/script.js
@@ -11,27 +11,12 @@ const emptyHourAndTextArray = [
     {hour: "1700", textValue: ""}
 ];
 var todaysEventsArray = {
-    date: "", // code todays date here
-    arrayHourAndText: emptyHourAndTextArray;
+    date: "", 
+    arrayHourAndText: emptyHourAndTextArray
 };
 const dateInHeader = document.getElementById("currentDay");
-const allButtons = document.getElementsByClassName('saveBtn'); // delete unless coded for
 const allTextAreas = document.getElementsByTagName('textarea');
 const allTimeBlocks = document.getElementsByClassName('time-block');
-const numberOfWorkHours = 9; //this needs to match the number of time blocks  // consider deleting
-var intervalLength = 1000; // I wanted this to be long enough that the function has time to reset this variable - one second should be plenty
-
-// var provideTimeInfo = function(format){
-//     switch (format) {
-//         case "date":
-//             return dayjs().format("YYYYMMDD");
-//         case "":
-//             dayjs().format("dddd, MMMM D");
-//         case "":
-
-//     }
-// }
-
 
 var createEmptyTodaysEventsArray = function(){
     todaysEventsArray.date = dayjs().format("YYYYMMDD");
@@ -40,7 +25,8 @@ var createEmptyTodaysEventsArray = function(){
 
 var uploadNewEvent = function(hour,text){
     // update todaysEventsArray
-    for ( i=0 ; i<numberOfWorkHours; i++ ){
+    debugger;
+    for ( i=0 ; i<allTimeBlocks.length; i++ ){
         if (todaysEventsArray.arrayHourAndText[i].hour == hour){
             todaysEventsArray.arrayHourAndText[i].textValue = text;
         }
@@ -54,9 +40,8 @@ var downloadTodaysEvents = function(){
     // check whether localStorage has today's events
     if ( localStorageDownload !== null ){
         localStorageDownload = JSON.parse(localStorageDownload);
-        if ( localStorageDownload.date == today ){
+        if ( localStorageDownload.date == dayjs().format("YYYYMMDD") ){
             todaysEventsArray = localStorageDownload;
-            populateStoredEventsToDOM(todaysEventsArray);
         } else {
             createEmptyTodaysEventsArray();
         }
@@ -69,16 +54,12 @@ var displayTodaysEvents = function(){
     //download calendar File
     downloadTodaysEvents();
     //populate dom with saved events, if any
-    for ( i = 0; i < numberOfWorkHours; i++ ){
+    for ( i = 0; i < allTimeBlocks.length; i++ ){
         if ( todaysEventsArray.arrayHourAndText[i].textValue ) {
             allTimeBlocks[i].children[1].value = todaysEventsArray.arrayHourAndText[i].textValue;
         }
     }
 }
-
-// var downloadCurrentTime = function(){
-    
-// }
 
 var addDateToHeader = function(){
     dateInHeader.innerText = dayjs().format( "dddd, MMMM D" )
@@ -87,78 +68,49 @@ var addDateToHeader = function(){
 var rightNowInMilitary = dayjs().format("HHmm");
 
 var colorTimeBlocks = function(){
-    rightNowInMilitary = "1130"; // remember to delete this
+    // this sub-function determines how much time is there to the next hour
     var intervalToNextColor = function() {
-        // how much time is there to the next hour; check second and minutes separately, then combine
-        var seconds = 60 - dayjs().format(ss);
-        var minutes = 60 - dayjs().format(mm);
-        // calculate the amount of time to the next hour plus two seconds to be sure that we are checking after the new hour has happened
+        // check second and minutes separately, then combine
+        var seconds = 60 - dayjs().format("ss");
+        var minutes = 60 - dayjs().format("mm");
+        // set interval to the amount of time to the next hour plus two seconds to be sure that we are checking after the new hour has happened
         var intervalInMilliseconds = ( 2000 + ( seconds * 1000 ) + ( minutes * 60 * 1000 ));
         return intervalInMilliseconds;
     }
-    // var intervalToNextColor = 2000; // placeholder 
     clearInterval(interval);
-    console.log("in colorTimeBlocks");    
-    console.log(rightNowInMilitary);
-    for ( i=0 ; i<numberOfWorkHours ; i++ ){
-        // replace with switch
-        if ( allTimeBlocks[i].children[0].id < rightNowInMilitary ){
-            console.log(allTimeBlocks[i].children[0].id + " is before now");
-            // insert class change here
+    for ( i=0 ; i<allTimeBlocks.length ; i++ ){
+        if ( allTimeBlocks[i].children[1].id < rightNowInMilitary ){
+            // color changes are made by changing the class
             allTimeBlocks[i].children[1].className = "col-9 past";
-            if ( ( rightNowInMilitary - allTimeBlocks[i].children[0].id ) < 60 ) {
+            if ( ( rightNowInMilitary - allTimeBlocks[i].children[1].id ) < 60 ) {
                 allTimeBlocks[i].children[1].className = "col-9 present";
             }
         } else {
-            console.log(allTimeBlocks[i].children[0].id + " is after now");
-            // if not in current hour, show as future, otherwise current
             allTimeBlocks[i].children[1].className = "col-9 future";
-            
         }
     }
-
-    //address transition to a new day
+    // reset upon transition to a new day
     if ( todaysEventsArray.date != dayjs().format("YYYYMMDD") ) {
+        addDateToHeader();
         displayTodaysEvents();
     };
-    
+    // update colors two seconds after the hour changes
     interval = setInterval( colorTimeBlocks, intervalToNextColor() );
 }
 
-// for(var i =0; i < allButtons.length; i++){
-//     allButtons[i].addEventListener('click', function(e){
-        
-//         const hour = this.parentNode.childNodes[1].innerText;
-//         const event = this.previousElementSibling;
-
-//         console.log(hour);
-
-//         console.log(event);
-     
-//         // grab the sibilibgs of the save button
-
-//         // save to localStorage
-//     })
-// }
-
 var newEvent = function(e){
-    console.log("blur");
-    console.log(e.target);
-    console.log(e.target.name);
-    console.log(e.target.value);
-    //user confirmation stuff - change button color or whatever
-    //add / replace existing event
-    uploadNewEvent("0900", e.target.value); //e.target.HOUR
-    //save to workingCalDate
-    //upload to localStorage
+    e.preventDefault();
+    // add / replace existing event
+    uploadNewEvent(e.target.id, e.target.value);
 }
 
-for ( i=0 ; i < allTextAreas.length; i++){
-    allTextAreas[i].addEventListener("blur", newEvent);
+var activateEventListenersForTextAreas = function() {
+    for ( i=0 ; i < allTextAreas.length; i++){
+        allTextAreas[i].addEventListener("blur", newEvent);
+    }
 }
 
 addDateToHeader();
 displayTodaysEvents();
 activateEventListenersForTextAreas();
-
-var interval = setInterval( colorTimeBlocks, intervalLength );
+var interval = setInterval( colorTimeBlocks, 0 );
